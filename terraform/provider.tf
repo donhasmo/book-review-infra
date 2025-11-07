743233f0b40f74c8daf6892e54600cbd00a1f14b
Hassan Hassan Ibrahim <103386326+donhasmo@users.noreply.github.com>
2025-11-07 07:58:53 +0100

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.110.0" # Stable version to prevent provider bugs
+    }
+  }
+
+  required_version = ">= 1.6.0"
+}
+
 provider "azurerm" {
   features {}
   subscription_id = "e0016351-6fec-419f-898c-796cbdf2c69c"
